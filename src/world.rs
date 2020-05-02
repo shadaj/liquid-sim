b02e71b81995f20f89fa5dc3b01f061bfb8fc0da
Nisreen Alqaisi <nisreenalqaisi@Nisreens-MacBook-Pro.local>
2020-05-02 10:15:35 -0500

    draft for self ocollisions

--- src/world.rs
+++ src/world.rs
@@ -2,13 +2,15 @@ use wasm_bindgen::prelude::*;
 
 use crate::particle::*;
 use std::panic;
+use std::collections::HashMap;
 
 extern crate console_error_panic_hook;
 
 #[wasm_bindgen]
 pub struct World {
   pub(crate) particles: Vec<Particle>,
-  
+  pub(crate) cell_size: f32,
+  pub(crate) particle_map: HashMap<f32, Vec<Particle>>
 }
 
 const GRAVITY: f32 = 0.5;
@@ -22,7 +24,9 @@ impl World {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     World {
-      particles: vec![]
+      particles: vec![],
+      cell_size: 0.1,
+      particle_map: HashMap::new()
     }
   }
 
@@ -75,12 +79,62 @@ impl World {
       }
     }
   }
+
+  fn build_spatial_map(&mut self){
+    // build hash map with a list of particles with the same hash value 
+    // (key = hash_position, value = list of particles)
+    self.particle_map.clear();
+    for particle in &self.particles {
+      let hash = self.hash_position(&particle);
+      match self.particle_map.get_mut(hash){
+        Some(&list) => list.push(particle),
+        None => self.particle_map.insert_mut(&hash, vec![particle])
+       }
+    }
+  }
+  fn self_collide(self, p: &Particle){
+    // calculate hash value and for all particles with the same hash value, 
+    // check if the particles collide
+    let key = self.hash_position(p.position);
+    let check = self.particles.get(key);
+    
+    // sudo code that needs to become rust code
+    // Vector3D correct = Vector3D();
+    // let mut count = 0;
+    // for(PointMass *c: check){
+    //     //check that it's not the point mass itself
+    //     if(c == &pm)
+    //         continue;
+    //     //calc for correction vector
+    //     Vector3D diffVec = pm.position - (*c).position;
+    //     double dist = diffVec.norm();
+    //     if(dist > (2.0 * thickness))
+    //         continue;
+    //     count += 1;
+    //     correct += (2 * thickness - dist) * diffVec.unit();
+    // }
+    // //average correction vector scaled down by simulation steps
+    // if(count == 0)
+    //     return;
+    // correct /= count;
+    // correct /= simulation_steps;
+    // pm.position = pm.position + correct;
+  }
+  fn hash_position(self, p: &Particle) -> f32 {
+    let width = 1.0/self.cell_size;
+    let grid_cell = (p.pos.x/self.cell_size).floor() + (width * (p.pos.y/self.cell_size).floor());
+    grid_cell
+  }
   
 
   pub(crate) fn update(&mut self, dt: f32) {
     self.apply_gravity();
     self.update_velocities(dt);
     self.update_positions(dt);
+    self.build_spatial_map();
+    for &mut particle in &mut self.particles{
+      self.self_collide(&particle)
+    }
     self.wall_collisions();
   }
 }
